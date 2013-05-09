f333515d097113b6fc55a0ba0b4395bf123e765c
vincent <vincent@vincent-PC>
2013-05-09 16:26:03 +0200

    premier commit...

---
+++ test/grails-app/controllers/test/Domain1Controller.groovy
@@ -0,0 +1,5 @@
+package test
+
+class Domain1Controller {
+    static scaffold = true
+}
---
+++ test/grails-app/domain/test/Domain1.groovy
@@ -0,0 +1,20 @@
+package test
+
+class Domain1 {
+
+	String texte
+	
+	int nombre
+	
+	float reel
+	
+	Date date
+	
+	byte[] fichier
+	
+	boolean condition
+	
+		
+    static constraints = {
+    }
+}
